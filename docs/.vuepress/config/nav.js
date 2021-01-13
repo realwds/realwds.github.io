4d0ca7f43510bfa56d6cad9f75bf2c29c101b9a0
Alejandro <wds@disnot.com>
2021-01-13 18:10:51 +0800

    add ssh

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,6 +1,28 @@
 module.exports = [
   { text: 'Home', link: '/' },
-  { text: 'About', link: '/about/' },  
+  { text: 'About', link: '/about/' },
+  {
+    text: 'Study',
+    link: '/study/',
+    items: [
+      {
+        text: 'SSH',
+        link: '/study/ssh/',
+        items: [
+          { text: 'SSH 基本知识', link: '/study/ssh/basic' },
+          { text: 'SSH 客户端', link: '/study/ssh/client' },
+          { text: 'SSH 密钥登录', link: '/study/ssh/key' },
+          { text: 'SSH 服务器', link: '/study/ssh/server' },
+          { text: '端口转发', link: '/study/ssh/port-forwarding' },
+          { text: 'SSH 证书登录', link: '/study/ssh/ca' },
+          { text: 'scp 命令', link: '/study/ssh/scp' },
+          { text: 'rsync 命令', link: '/study/ssh/rsync' },
+          { text: 'sftp 命令', link: '/study/ssh/sftp' },
+        ]
+      }
+    ]
+    
+  },
   { 
     text: 'More', 
     link: '/more/',
