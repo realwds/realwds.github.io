aa8d281b158518c2b7451a37dd96e145a8e701c5
Alejandro <wds@disnot.com>
2021-01-13 23:47:18 +0800

    edit ssh

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,6 +1,5 @@
 module.exports = [
   { text: 'Home', link: '/' },
-  { text: 'About', link: '/about/' },
   {
     text: 'Study',
     link: '/study/',
@@ -21,37 +20,14 @@ module.exports = [
         ]
       }
     ]
-    
   },
   { 
     text: 'More', 
     link: '/more/',
     items: [
-      { text: '奇技淫巧', link: '/more/one' },
-      { text: 'two', link: '/more/two' },
-      { text: 'three', link: '/more/three' }
+      { text: 'GitHub 国内加速', link: '/more/one' },
+      { text: 'GitPage 三合一部署', link: '/more/two' },
     ]
   },
-  { 
-    text: 'GitResources', 
-    items: [
-      { 
-        text: 'GitPage', 
-        items: [
-          { text: 'Gitee Page', link: 'https://realwds.gitee.io/' },
-          { text: 'GitHub Page', link: 'https://realwds.github.io/' },
-          { text: 'GitLab Page', link: 'https://realwds.gitlab.io/' }
-        ]
-      },
-      { 
-        text: 'GitStore', 
-        items: [
-          { text: 'Gitee Store', link: 'https://gitee.com/realwds/' },
-          { text: 'GitHub Store', link: 'https://github.com/realwds/' },
-          { text: 'GitLab Store', link: 'https://gitlab.com/realwds/' }
-        ]
-      },
-      
-    ]
-  }
+  { text: 'About', link: '/about/' }
 ]
