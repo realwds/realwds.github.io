193bd1674dae56cc92baf6bec876759611c23581
Alejandro <wds@disnot.com>
2021-01-28 16:40:44 +0800

    sidebar group

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -2,31 +2,45 @@ module.exports = {
   "/zh/website/": ["../../zh/website/"],
   "/zh/article/": ["../../zh/article/"],
   "/zh/study/git/": [
-    "../../study/git/1-1.常用Git命令清单",
-    "../../study/git/1-1.常用Git命令清单",
-    "../../study/git/1-2.Git变基合并",
-    "../../study/git/1-3.Git命令思维导图",
-    "../../study/git/2-1.Git基础与命令",
-    "../../study/git/2-2.Git分支-分支原理",
-    "../../study/git/2-3.Git分支的新建与合并-分支操作",
-    "../../study/git/2-4.Git分支管理-查看分支",
-    "../../study/git/2-5.Git分支开发工作流",
-    "../../study/git/2-6.Git分支-远程分支",
-    "../../study/git/2-7.Git分支-变基",
-    "../../study/git/2-8.Git工具-查看修订版本",
-    "../../study/git/2-9.Git工具-交互式暂存",
-    "../../study/git/2-10.Git工具-重写历史",
-    "../../study/git/2-11.Git工具-重置揭密"	
+    {
+      title: 'Git 目录',   // 必要的
+      //path: '/zh/study/git/',  // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      collapsable: false, // 可选的, 默认值是 true,
+      sidebarDepth: 1,    // 可选的, 默认值是 1
+      children: [
+        "../../study/git/1-1.常用Git命令清单",
+        "../../study/git/1-2.Git变基合并",
+        "../../study/git/1-3.Git命令思维导图",
+        "../../study/git/2-1.Git基础与命令",
+        "../../study/git/2-2.Git分支-分支原理",
+        "../../study/git/2-3.Git分支的新建与合并-分支操作",
+        "../../study/git/2-4.Git分支管理-查看分支",
+        "../../study/git/2-5.Git分支开发工作流",
+        "../../study/git/2-6.Git分支-远程分支",
+        "../../study/git/2-7.Git分支-变基",
+        "../../study/git/2-8.Git工具-查看修订版本",
+        "../../study/git/2-9.Git工具-交互式暂存",
+        "../../study/git/2-10.Git工具-重写历史",
+        "../../study/git/2-11.Git工具-重置揭密"	
+      ]
+    }
   ],
   "/zh/study/ssh/": [
-    "../../study/ssh/basic",
-    "../../study/ssh/client",
-    "../../study/ssh/key",
-    "../../study/ssh/server",
-    "../../study/ssh/port-forwarding",
-    "../../study/ssh/ca",
-    "../../study/ssh/scp",
-    "../../study/ssh/rsync",
-    "../../study/ssh/sftp"
-  ],
+    {
+      title: 'SSH 目录',   // 必要的
+      collapsable: false, // 可选的, 默认值是 true,
+      sidebarDepth: 1,    // 可选的, 默认值是 1
+      children: [
+        "../../study/ssh/basic",
+        "../../study/ssh/client",
+        "../../study/ssh/key",
+        "../../study/ssh/server",
+        "../../study/ssh/port-forwarding",
+        "../../study/ssh/ca",
+        "../../study/ssh/scp",
+        "../../study/ssh/rsync",
+        "../../study/ssh/sftp"
+      ]
+    }
+  ]
 }
